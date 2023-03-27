eb514a2bcd0d6e6deefbc6a789ad666b32fa730e
MMosayebi19 <128969996+MMosayebi19@users.noreply.github.com>
2023-03-27 11:56:02 +0330

    SQL Project

---
+++ games.sql
@@ -0,0 +1,27 @@
+select * from [Athletes Dataset].[games].[Athletes_summer_games]
+
+select * from [Athletes Dataset].[games].[Athletes_winter_games]
+
+select * from [Athletes Dataset].[games].[regions]
+
+-- Q1 : Number of sports in per season
+-- first : winter
+select Season , count(Sport) as 'Number of sports'
+from [Athletes Dataset].[games].[Athletes_winter_games]
+group by Season 
+--
+select Season , Sport ,count(Sport) as 'Number of sports'
+from [Athletes Dataset].[games].[Athletes_winter_games]
+group by Season , Sport
+-- second : summer
+select Season , count(Sport) as 'Number of sports'
+from [Athletes Dataset].[games].[Athletes_summer_games]
+group by Season 
+--
+select Season , Sport ,count(Sport) as 'Number of sports'
+from [Athletes Dataset].[games].[Athletes_summer_games]
+group by Season , Sport
+
+-- Q2 : Top 10 of the most participants of sport per city
+
+select 
